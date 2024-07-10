b768bc38ea0e19997251db0b8ebaf8018c40b348
denys.zagrebelnyi <denys.zagrebelnyi@mgid.com>
2024-07-10 17:17:31 +0300

    hw17.1

--- cypress/support/steps/MainPageSteps.js
+++ cypress/support/steps/MainPageSteps.js
@@ -2,19 +2,19 @@ import mainePage from '../pages/MainPage';
 
 const MainPageSteps = {
   CheckingButtonTagsInHeader() {
-    mainePage.getAboutHeaderBtn();
-    mainePage.getContactsHeaderBtn();
-    mainePage.getGuestLogInHeaderBtn();
-    mainePage.getSignInHeaderBtnn;
+    mainePage.getAboutHeaderBtn().clickElement();
+    mainePage.getContactsHeaderBtn().clickElement();
+    mainePage.getGuestLogInHeaderBtn()
+    mainePage.getSignInHeaderBtnn().clickElement();
   },
   CheckingLinksInFooter() {
-    mainePage.getFacebookSelector(); //.should('have.attr', 'href', mainePage.getFacebookLink())
-    mainePage.getTelegramSelector(); //.should('have.attr', 'href', mainePage.getTelegramLink())
-    mainePage.getYoutubeSelector(); //.should('have.attr', 'href', mainePage.getYoutubeLink())
-    mainePage.getInstagramSelector(); //.should('have.attr', 'href', mainePage.getInstagramLink())
-    mainePage.getLinkedinSelector(); //.should('have.attr', 'href', mainePage.getLinkedinLink())
-    mainePage.getIthillelLogoSelector(); //.should('have.attr', 'href', mainePage.getIthillelLogoLink())
-    mainePage.getMailtoMailSelector(); //.should('have.attr', 'href', mainePage.getMailtoMail())
+    mainePage.getFacebookSelector().isVisible(); 
+    mainePage.getTelegramSelector().isVisible(); 
+    mainePage.getYoutubeSelector().isVisible(); 
+    mainePage.getInstagramSelector().isVisible(); 
+    mainePage.getLinkedinSelector().isVisible(); 
+    mainePage.getIthillelLogoSelector().isVisible();
+    mainePage.getMailtoMailSelector().isVisible(); 
   },
 };
 
